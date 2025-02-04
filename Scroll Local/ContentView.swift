eed80f8ae135b24ae191d86c759c26acbb1c2675
Eli-Gooding <eli.w.gooding@gmail.com>
2025-02-03 18:14:51 -0600

    Initial Commit

---
+++ Scroll Local/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Scroll Local
+//
+//  Created by Eli Gooding on 2/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Scroll Local/Scroll_LocalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Scroll_LocalApp.swift
+//  Scroll Local
+//
+//  Created by Eli Gooding on 2/3/25.
+//
+
+import SwiftUI
+
+@main
+struct Scroll_LocalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
