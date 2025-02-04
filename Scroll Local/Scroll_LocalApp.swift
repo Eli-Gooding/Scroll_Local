1a03e7eb4f878963e22e474860d7103938cc1e56
Eli-Gooding <eli.w.gooding@gmail.com>
2025-02-04 10:25:44 -0600

    basic firebase dependencies

--- Scroll Local/Scroll_LocalApp.swift
+++ Scroll Local/Scroll_LocalApp.swift
@@ -6,12 +6,21 @@
 //
 
 import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
 
 @main
 struct Scroll_LocalApp: App {
     // TODO: Re-enable when implementing Firebase
     // Register app delegate for Firebase setup
-    // @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     
     var body: some Scene {
         WindowGroup {
@@ -19,3 +28,5 @@ struct Scroll_LocalApp: App {
         }
     }
 }
+
+
